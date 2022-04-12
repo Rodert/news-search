f6b4dbfdf5dfa4d0fe4bbae9dc2711ca8c38b8db
wsy <wangshiyu@ai-finance.cn>
2022-04-12 15:25:28 +0800

    news search init

---
+++ news-search-web/src/main/java/com/javapub/newssearch/newssearchweb/NewsSearchWebApplication.java
@@ -0,0 +1,13 @@
+package com.javapub.newssearch.newssearchweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSearchWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsSearchWebApplication.class, args);
+    }
+
+}
